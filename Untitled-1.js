738dcfbb3e919919ae1969f52f5f074fba9ada3e
xays <cppxays@gmail.com>
2018-08-15 16:21:13 +0300

    added console log

---
+++ Untitled-1.js
@@ -0,0 +1,18 @@
+// var host = {
+//     ip: "127.0.0.1",
+//         user:{
+//             name: "root",
+//             password: "123"
+//         }
+// };
+// console.log(host.user["name"]);
+
+// ---------------
+// var user = {
+//     name: "root"
+// }
+
+// var x = user;
+// x.name = "root2";
+// console.log(user.name);
+console.log("test_uploadd");
