b2badc7436d7c381e4d6d9546b69c5b52b4860f8
xays <cppxays@gmail.com>
2018-08-16 03:06:53 +0300

    добавлен литерал одноуровневый

---
+++ literals.js
@@ -0,0 +1,12 @@
+// использование литералов
+var server = {
+    host: "127.0.0.1",
+    user: "root",
+    password: "123"
+};
+
+// вывод в консоль
+console.log(server["host"], 
+            server["user"], 
+            server["password"]
+           );
