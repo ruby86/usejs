5f20d1d8ddc43fa26b0550a23dd96a7325e65a67
xays <cppxays@gmail.com>
2018-08-16 16:06:31 +0300

    добавилась собака

--- prototips.js
+++ prototips.js
@@ -13,6 +13,7 @@ var Animal = {                   //объявлен объект который 
 };
 
 var Cat = Object.create(Animal);
+var Dog = Object.create(Animal);
 
 Cat.constructor = function(name, age, type, color){
     Animal.constructor.apply(this, arguments);
@@ -20,5 +21,13 @@ Cat.constructor = function(name, age, type, color){
 return this;
 }
 
+Dog.constructor = function(name, age, type, color){
+    Animal.constructor.apply(this, arguments);
+        this.color = color;
+return this;
+}
+
 var cat = Object.create(Cat).constructor("Messi", 10, "кошка", "красный");
+var dog = Object.create(Dog).constructor("Monny", 15, "собачка", "черный");
 cat.privetstvie();
+dog.privetstvie();
