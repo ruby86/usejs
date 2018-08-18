cc07ca18beedc2c22b542d9bc49d91b5a5346e4a
xays <cppxays@gmail.com>
2018-08-18 16:26:02 +0300

    добавлены услывные операторы if else if & swith

---
+++ uslovnii_operator_if.js
@@ -0,0 +1,19 @@
+var count = 1;
+    if(count == 1){
+        console.log("count = 1");
+    }
+    else if(count == 3){
+        console.log("count = 3");
+    }
+    else{
+        console.log("count != 1");
+    };
+
+var count_switch = 2;
+    switch(count_switch){
+    case 1: count_switch = "count = 1"; break;
+    case 2: count_switch = "count = 2"; break;
+    case 3: count_switch = "count = 3";
+    default: "undefinde count";
+    }
+        console.log(count_switch);
