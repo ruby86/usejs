f39732538a1a430f49ccde23f1912316fff58baf
xays <cppxays@gmail.com>
2018-08-18 16:02:10 +0300

    добавлены три вида циклов for, while, do

---
+++ cikli.js
@@ -0,0 +1,22 @@
+// цикл for
+
+    var i;
+    for(i = 0; i < 10; i++){
+        console.log("for: " + i);
+    };
+
+// цикл while
+
+var i = 0;
+    while(i < 10){
+        console.log("while", i);
+        i++;
+    }
+
+// цикл do
+
+var i = 0
+do{
+    console.log("do", i);
+    i++;
+}while(i < 10); 
